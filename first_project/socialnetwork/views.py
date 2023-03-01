4096c003a300858e318efcf433bd79fe43c1733a
mohammadnpak <m.nozaripak@gmail.com>
2023-03-01 20:45:34 +0330

    s07-b

--- first_project/socialnetwork/views.py
+++ first_project/socialnetwork/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,User
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request,'socialnetwork/post_list.html',context={'posts':posts})
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(request,'socialnetwork/post_list.html',context={'posts':posts})
+    
+    elif request.method == "POST":
+        # do something
+       
+        new_post_body = request.POST["new_post_body"]
+        new_post_title = request.POST["new_post_title"]
+        Post.objects.create(
+            body=new_post_body,
+            author=User.objects.get(id=1),
+            title=new_post_title)
+
+
+        posts = Post.objects.all()
+        return render(request,'socialnetwork/post_list.html',context={'posts':posts})
+
 
 def post_detail(request,pk):
   
