2628a7b6fd01c4155c473251bd4c150f56a389d3
mohammadnpak <m.nozaripak@gmail.com>
2023-03-15 18:38:25 +0330

    s10a

---
+++ TRI.py
@@ -0,0 +1,12 @@
+info = [{},{},{}]
+name_book = input('set your name book:')
+authoy = input('set your authoy:')
+date = input('set your date age')
+info[0]['name_book']
+info[1]['authoy']
+info[2]['date']
+
+ 
+print('info')
+
+
--- first_project/socialnetwork/views.py
+++ first_project/socialnetwork/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def post_list(request):
     posts = Post.objects.all()
     return render(request,'socialnetwork/post_list.html',context={'posts':posts})
 
+@login_required
 def post_detail(request,pk):
-  
     p = get_object_or_404(Post,pk=pk)
     return render(request,'socialnetwork/post_detail.html',{'post':p})
